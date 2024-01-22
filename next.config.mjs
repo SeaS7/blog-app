cd1c614f67cb468537fe0cede0695b59910453f1
Muhammad Huzaifa <dev.muhammadhuzaifa@gmail.com>
2024-01-22 22:24:24 +0500

    added authentication class

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env : {
+        appwriteUrl : String(process.env.NEXT_APPWRITE_URL),
+        appwriteProjectId : String(process.env.NEXT_APPWRITE_PROJECT_ID),
+        appwriteDataBaseId : String(process.env.NEXT_APPWRITE_DATABASE_ID),
+        appwriteCollectionId : String(process.env.NEXT_COLLECTION_ID),
+        appwriteBucketId : String(process.env.NEXT_BUCKET_ID),
+    }
+};
 
 export default nextConfig;
